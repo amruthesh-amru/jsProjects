505da3131d2878f298dffdc6f46b8b2edc2313fc
amruthesh <amrutheshamru16@gmail.com>
2023-07-19 00:43:03 +0530

    major changes

--- Image fetch/script.js
+++ Image fetch/script.js
@@ -1,10 +1,9 @@
-const accessKey = "jRhdVnnf8w0G8Oon-m_oWNCzxMKbeEkCPkIPzOZlou0";
-
+const accessKey = "3BDVH52HqXTsqNR4eHGXHXi9TJBWKuiIrbGxfwbeinI";
 const formEl = document.querySelector("form");
 const inputEl = document.getElementById("search_input");
 const searchResults = document.querySelector(".container");
 const showMore = document.getElementById("show_more_button");
-const searchBtnEl = document.getElementById("search_button");
+
 
 
 let inputData = "";
@@ -12,11 +11,12 @@ let page = 1;
 
 
 async function searchImages() {
-    inputData = inputEl.ariaValueMax;
-    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`
+    inputData = inputEl.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
 
     const response = await fetch(url)
     const data = await response.json();
+    console.log(data);
     const results = data.results
 
     if (page === 1) {
@@ -27,12 +27,31 @@ async function searchImages() {
         const imageWrapper = document.createElement("div");
         imageWrapper.classList.add("card");
         const image = document.createElement("img")
-        image.src = results.urls.small
+        image.src = result.urls.small
         image.alt = result.alt_description
-        const imageLink = document.createElement('a')
+        const imageLink = document.createElement("a")
         imageLink.href = results.links.html
         imageLink.target = "_blank"
         imageLink.textContent = result.alt_description
+
+
+        imageWrapper.appendChild(image)
+        imageWrapper.appendChild(imageLink)
+        searchResults.appendChild(imageWrapper)
     })
+
+    page++
+
+    if (page > 1) {
+        showMore.style.display = "block"
+    }
 }
 
+formEl.addEventListener("submit", (event) => {
+    event.preventDefault()
+    page = 1;
+    searchImages()
+})
+showMore.addEventListener("click", () => {
+    searchImages()
+})
