8dac2aaae2a3524474a8971f4674352be5629de6
amruthesh <amrutheshamru16@gmail.com>
2023-07-31 20:45:09 +0530

    minor changes in Qr Generator

--- QR Generator/script.js
+++ QR Generator/script.js
@@ -1,10 +1,22 @@
-const generateBtn = document.getElementById("generateBtn")
-const form = document.getElementById("form")
-const qrImage = document.getElementById("qrImage")
-const loadingText = document.getElementById("loading")
-const qrSection = document.getElementsByClassName("qrSection")
+const generateBtn = document.getElementById("generateBtn");
+const form = document.getElementById("form");
+const qrImage = document.getElementById("qrImage");
+const loadingText = document.getElementById("loading");
+const qrSection = document.getElementById("qrSection");
 
 // const url = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${text}"
+
+generateQr = (url) => {
+    loadingText.classList.remove("hide");
+    qrImage.src = url;
+    setInterval(() => {
+        qrSection.classList.remove("hide");
+        qrSection.classList.add("show");
+        loadingText.classList.add("hide");
+    }, 500);
+
+}
+
 generateBtn.addEventListener("click", (event) => {
 
     event.preventDefault();
@@ -12,9 +24,6 @@ generateBtn.addEventListener("click", (event) => {
     const formData = new FormData(form)
     const text = formData.get("text");
 
-    qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${text}`
-
-    qrSection.classList.add("show")
-
-
+    url = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${text}`
+    generateQr(url);
 })
